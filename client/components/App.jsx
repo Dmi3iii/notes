5c37d39291fb61824221d811869f399f61e3a865
Dm_i3 <kaleev@mail.ru>
2018-09-26 23:53:40 +0300

    добавляем работу с API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import NoteEditor from './NoteEditor.jsx';
 import NotesGrid from './NotesGrid.jsx';
 
-class App extends React.Component{
+import './App.less';
+
+class App extends Component{
+
+    handleNoteAdd(data){
+      console.log(data);
+    }
+
     render(){
       return (
         <div className="App">
           <h2 className="App__header">NotesApp</h2>
-          <NoteEditor />
+          <NoteEditor onNoteAdd={this.handleNoteAdd}/>
           <NotesGrid />
         </div>
         );
--- client/components/NoteEditor.jsx
+++ client/components/NoteEditor.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './NoteEditor.less';
 
-class NoteEditor extends React.Component{
+class NoteEditor extends Component{
   constructor(props){
     super(props);
     this.state={
@@ -10,10 +10,13 @@ class NoteEditor extends React.Component{
       text:'',
       color:'#FFFFFF'
     };
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleNoteAdd = this.handleNoteAdd.bind(this);
   }
 
-  handleTextChange(event){
-    this.setState({title: event.target.value});
+  handleTextChange(event) {
+    this.setState({text: event.target.value});
   }
 
   handleTitleChange(event){
@@ -27,7 +30,7 @@ class NoteEditor extends React.Component{
       color: this.state.color
     };
 
-    //this.props.onNoteAdd(newNote);
+    this.props.onNoteAdd(newNote);
     this.setState({text:'',title:'',color:'#FFFFFF'});
   }
 
@@ -37,21 +40,21 @@ class NoteEditor extends React.Component{
         <input
           type='text'
           className='NoteEditor__title'
-          plaseholder='Enter title'
+          placeholder='Enter title'
           value={this.state.title}
-          onChange={this.state.handleTitleChange}
+          onChange={this.handleTitleChange}
         />
         <textarea
             className='NoteEditor__text'
-            plaseholder='Enter note text'
+            placeholder='Enter note text'
             rows={5}
             value={this.state.text}
-            onChange={this.state.handleTextChange}
+            onChange={this.handleTextChange}
         />
         <div className='NoteEditor__footer'>
           <button
             className='NoteEditor__button'
-            disabled={!this.state.text}
+            disabled={!(this.state.text && this.state.title)}
             onClick={this.handleNoteAdd}
           >
             Add
