dea62108946083d3155ebd0a819eef7e7a3d782d
Dm_i3 <kaleev@mail.ru>
2018-09-27 17:01:02 +0300

    добавлена работа с БД, но пока есть ошибки

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,55 @@
 import React, {Component} from 'react';
 
+import NotesStore from '../stores/NotesStore'
+import NotesActions from '../actions/NotesActions';
+
 import NoteEditor from './NoteEditor.jsx';
 import NotesGrid from './NotesGrid.jsx';
 
 import './App.less';
 
+function getStateFromFlux(){
+  return {
+    isLoading: NotesStore.isLoading(),
+    notes: NotesStore.getNotes()
+  };
+}
+
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state=getStateFromFlux();
+  }
 
-    handleNoteAdd(data){
-      console.log(data);
-    }
+  componentWillMount(){
+      NotesActions.loadNotes();
+  }
 
-    render(){
-      return (
-        <div className="App">
-          <h2 className="App__header">NotesApp</h2>
-          <NoteEditor onNoteAdd={this.handleNoteAdd}/>
-          <NotesGrid />
-        </div>
-        );
-    }
+  componentDidMount(){
+    NotesStore.addChangeLisener(this._onChange);
+  }
+
+  componentWillUnmount(){
+    NotesStore.removeChangeLisener(this._onChange);
+  }
+
+  handleNoteAdd(data){
+    NotesActions.createNote(data);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h2 className="App__header">NotesApp</h2>
+        <NoteEditor onNoteAdd={this.handleNoteAdd}/>
+        <NotesGrid />
+      </div>
+      );
+  }
+
+  _onChange(){
+    this.setState(getStateFromFlux());
+  }
 }
 
 export default App;
